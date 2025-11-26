6ae83cf36a1df7c9c98685a645c071ac16e3c614
krishnaprasad <krishnaprasad.m@devopslabs.tech>
2025-11-26 20:16:45 +0530

    Secure, Event-Driven Microservices with Caching

---
+++ src/main/java/employee-leave-management-system/controller/EmployeeController.java
@@ -0,0 +1,40 @@
+package employeeLeaveManagementSystem.controller;
+
+import employeeLeaveManagementSystem.dto.EmployeeDTO;
+import employeeLeaveManagementSystem.service.EmployeeService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/api/v1/employeeDetails")
+public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService){
+        this.employeeService = employeeService;
+    }
+
+
+    //Save Or Update the Employee Details :
+    @PostMapping("/saveOrUpdate")
+    public ResponseEntity<String> saveOrUpdate(@RequestBody EmployeeDTO dto) {
+        return employeeService.saveOrUpdate(dto);
+    }
+
+
+    //To List the leave balance :
+    @GetMapping("/leaveBalanceCheck/{id}")
+    public ResponseEntity<?> getLeaveBalance(@PathVariable Long id) {
+        return employeeService.getLeaveBalance(id);
+    }
+
+    @GetMapping("/getCachedBalance/{id}")
+    public ResponseEntity<?> getBalance(@PathVariable Long id) {
+        EmployeeDTO dto = employeeService.getCachedBalance(id);
+        return ResponseEntity.ok(dto);
+    }
+
+
+}
